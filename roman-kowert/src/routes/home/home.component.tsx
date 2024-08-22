0d5ff046d02bb64bd85e6197be8ca8f145c78afa
QuentinAyers1917 <rkowert@posteo.de>
2024-08-22 15:31:59 +0200

    start working on hover preview in navigation component

--- roman-kowert/src/routes/home/home.component.tsx
+++ roman-kowert/src/routes/home/home.component.tsx
@@ -3,8 +3,9 @@ import React from "react";
 const Home = () => {
   return (
     <div className="h-full flex items-center">
-      <div className="w-96 text-xl block">
-        Bringing the creativity of Frontend Development together with the
+      <div className="w-96 text-xl text-right block leading-10">
+        Hello. I'm Roman, a Fullstack Web Developer versed in React, Node.js and Python.
+        My ideal is to bring the creativity of Frontend Development together with the
         technical ingenuity of Backend Development.
       </div>
     </div>
--- roman-kowert/src/routes/navigation/navigation.component.tsx
+++ roman-kowert/src/routes/navigation/navigation.component.tsx
@@ -1,15 +1,45 @@
+import { useState } from "react";
 import { Fragment } from "react/jsx-runtime";
 import { Outlet } from "react-router-dom";
 
+type previewProps = string;
+
 const Navigation = () => {
+  const [showPreview, setShowPreview] = useState({
+    about: false,
+    project: false,
+    contact: false,
+  });
+
+  const handlePreview = (page: previewProps) => {
+    console.log(page);
+  };
+
   return (
     <Fragment>
       <div className="w-fit h-[90vh] flex items-center">
         <div className="flex flex-col items-start gap-3">
-          <div className="text-5xl font-extrabold mb-4">Roman Kowert</div>
-          <div className="text-xl font-bold">About</div>
-          <div className="text-xl font-bold">Projects</div>
-          <div className="text-xl font-bold">Contact</div>
+          <div className="text-6xl font-extrabold mb-1">Roman Kowert</div>
+          <div className="text-2xl font-bold mb-4">Fullstack Web Engineer</div>
+          <div
+            className="text-xl font-bold hover:text-red-400 hover:translate-x-4 transition-all cursor-pointer"
+            onMouseOver={() => handlePreview("about")}
+          >
+            About{}
+          </div>
+          <div className="hidden">Preview of About Page</div>
+          <div
+            className="text-xl font-bold hover:text-red-400 hover:translate-x-4 transition-all cursor-pointer"
+            onMouseOver={() => handlePreview("projects")}
+          >
+            Projects
+          </div>
+          <div className="hidden">Preview of Projects Page</div>
+          <div className="text-xl font-bold hover:text-red-400 hover:translate-x-4 transition-all cursor-pointer"
+            onMouseOver={() => handlePreview("contact")}
+          >
+            Contact
+          </div>
         </div>
       </div>
       <Outlet />
