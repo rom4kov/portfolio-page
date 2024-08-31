2ceb957ac800070ccf38eb58bd12a9a6945a45ec
QuentinAyers1917 <rkowert@posteo.de>
2024-08-31 23:06:12 +0200

    create user model

---
+++ server/models.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+from flask_login import UserMixin
+from sqlalchemy import Integer, String, Table, Column
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.sql.elements import SQLCoreOperations
+from main import db
+from typing import List, Dict, Any, Optional
+
+
+class User(db.Model, UserMixin): # type: ignore[name-defined]
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
