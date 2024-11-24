437880e7365349023406f47dd788bc0fff50d5a7
QuentinAyers1917 <rkowert@posteo.de>
2024-11-24 01:16:36 +0100

    change postinstall.js so it works with Node and Docker

--- roman-kowert/postinstall.js
+++ roman-kowert/postinstall.js
@@ -1,5 +1,23 @@
-import fse from 'fs-extra';
+import { fileURLToPath } from 'url';
 import path from 'path';
-const topDir = import.meta.dirname;
-fse.emptyDirSync(path.join(topDir, 'public', 'tinymce'));
-fse.copySync(path.join(topDir, 'node_modules', 'tinymce'), path.join(topDir, 'public', 'tinymce'), { overwrite: true });
+
+// Get the current file's directory
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Use __dirname instead of import.meta.dirname
+fse.emptyDirSync(path.join(__dirname, 'public', 'tinymce'));
+fse.copySync(
+  path.join(__dirname, 'node_modules', 'tinymce'),
+  path.join(__dirname, 'public', 'tinymce'),
+  { overwrite: true }
+);
+
+
+// This worked locally, but does not work with Docker:
+
+// import fse from 'fs-extra';
+// import path from 'path';
+// const topDir = import.meta.dirname;
+// fse.emptyDirSync(path.join(topDir, 'public', 'tinymce'));
+// fse.copySync(path.join(topDir, 'node_modules', 'tinymce'), path.join(topDir, 'public', 'tinymce'), { overwrite: true });
