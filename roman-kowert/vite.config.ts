b006434aa10d5fe9da432affbe689789c0cc7f9b
QuentinAyers1917 <rkowert@posteo.de>
2024-11-30 01:27:07 +0100

    remove root directory node_modules from git for good

--- roman-kowert/vite.config.ts
+++ roman-kowert/vite.config.ts
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react";
 import compression from "vite-plugin-compression2";
 import { ViteMinifyPlugin } from "vite-plugin-minify";
 
-
-
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   const port = env.REACT_APP_ENV === "development" ? 5173 : 4173;
